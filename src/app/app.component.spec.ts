680e47bb40bfdf48b3ddce269e5d589a0a731a42
Claudionor Pontes <claudionor.pontes@outlook.com>
2023-12-22 18:32:04 -0300

    push

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,9 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-import { Title } from '@angular/platform-browser';
-import { loadSinglePageError } from './store/singlepage.actions';
-
-describe('AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
-  let appComponent: AppComponent;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent], // Adicione o seu componente aqui, se necessário
-      providers: [
-        Title,
-        Store // Adicione o seu serviço de store aqui, se necessário
-      ]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(AppComponent);
-    appComponent = fixture.componentInstance;
-  });
-
-  it(`should have the 'singlepage' title`, () => {
-    const titleService = TestBed.inject(Title);
-
-    // Simule a ação que define o título no seu componente
-    appComponent.ngOnInit(); // Chame o método que define o título, se existir
-
-    expect(titleService.getTitle()).toBe('singlepage');
-  });
-
-  // Adicione outros testes conforme necessário
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/caminho-para-seu-arquivo/service-worker.js')
+    .then(registration => {
+      console.log('Service Worker registrado com sucesso:', registration);
+    })
+    .catch(error => {
+      console.error('Erro ao registrar o Service Worker:', error);
+    });
+}
